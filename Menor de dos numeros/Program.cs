3236596816b6bc4b6a6e0d3188d7168fe2fb4a45
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 22:35:04 -0300

    Menor de dos numeros enteros

--- Menor de dos numeros/Program.cs
+++ Menor de dos numeros/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Menor_de_dos_numeros
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Menor(int a,int b)
         {
-            Console.WriteLine("Hello World!");
+            // realizamos la operaciones para saber cual es el menor
+            if (a < b)
+                Console.WriteLine("el numero menor es: {0}", a);
+            else
+                Console.WriteLine("el numero menor es: {0}", b);
+        }
+        public static void Main()
+        {
+            //declaracion de las variables, 
+            int a, b;
+            // interacion con el usuario
+            Console.WriteLine("Escribe un numero entero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe otro numero entero: ");
+            b= Convert.ToInt32(Console.ReadLine());
+
+            // llamado a a la funcion
+            Menor(a, b);
+            Console.ReadKey();
         }
     }
 }
